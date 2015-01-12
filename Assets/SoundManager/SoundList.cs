ccecb1047df38724f6e63f01865a5a3f9f311199
adarapata <efb.youdie@gmail.com>
2015-01-12 23:47:00 +0900

    Enumの値によって配列が可変する音楽リストスクリプト作成

---
+++ Assets/SoundManager/SoundList.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class SoundList : ScriptableObject {
+    private AudioClip[] audioList = new AudioClip[0];
+
+    public AudioClip this[int i]
+    {
+        get { return audioList [i]; }
+        set { audioList [i] = value; }
+    }
+
+    void OnEnable()
+    {
+        int enumLength = Enum.GetNames (typeof(GameSound)).Length;
+        if (audioList.Length == enumLength)
+            return;
+
+        AudioClip[] swap = audioList.Clone() as AudioClip[];
+        audioList = new AudioClip[enumLength];
+        for (int i = 0; i < audioList.Length; i++) {
+            audioList [i] = swap [i];
+        }
+    }
+}
