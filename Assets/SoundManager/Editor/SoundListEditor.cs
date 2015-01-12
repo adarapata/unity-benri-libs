acc776f9f50345cee5de1957b63fac549fe80521
adarapata <efb.youdie@gmail.com>
2015-01-12 23:47:49 +0900

    サウンドリストの表示を変えるエディタ拡張

---
+++ Assets/SoundManager/Editor/SoundListEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+[CustomEditor (typeof(SoundList))]
+public class SoundListEditor : UnityEditor.Editor
+{
+    public override void OnInspectorGUI ()
+    {
+        string[] names = Enum.GetNames (typeof(GameSound));
+        var soundList = target as SoundList;
+        for (int i = 0; i < names.Length; i++) {
+            soundList [i] = EditorGUILayout.ObjectField (names [i], soundList [i], typeof(AudioClip)) as AudioClip;
+        }
+    }
+}
