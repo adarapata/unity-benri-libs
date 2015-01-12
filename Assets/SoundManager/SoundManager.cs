d1fe91ba5999e8cc83c7b07a52de089c253d38f7
adarapata <efb.youdie@gmail.com>
2015-01-12 23:48:14 +0900

    音声再生スクリプト

---
+++ Assets/SoundManager/SoundManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoundManager : MonoBehaviour
+{
+    public SoundList soundList;
+
+    public void Play(GameSound soundName)
+    {
+        GameObject sound = new GameObject (soundName.ToString ());
+        sound.AddComponent<AudioSource> ();
+        sound.audio.clip = soundList [(int)soundName];
+        sound.audio.Play ();
+        Destroy (sound, 3F);
+    }       
+}
